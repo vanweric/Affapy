8484d2a0573797496d959a29a7217d0883e3e401
Quentin18 <quentindeschamps18@orange.fr>
2020-04-01 23:10:23 +0200

    Modif tests d'après nouvelle structure

--- test/test_affine.py
+++ test/test_affine.py
@@ -7,20 +7,19 @@ import unittest
 class TestAffine(unittest.TestCase):
     """Test case used to test functions from class Affine"""
 
-    def test_add_affine(self):   #TODO : edit function 'add' from Affine to consider two same epsilon
+    def test_add_affine(self):
         """Test 'add' function from class Affine"""
-        x = Affine({0: 0, 1: 10})
-        y = Affine({0: 5, 1: 10, 2: 5})
-        self.assertEqual(x + y, Affine({0: 5, 1: 20, 2: 5}))
-        self.assertEqual(x + x, Affine({0: 0, 1: 20}))
-
+        x = Affine(0, {1: 10})
+        y = Affine(5, {1: 10, 2: 5})
+        self.assertEqual(x + y, Affine(5, {1: 20, 2: 5}))
+        self.assertEqual(x + x, Affine(0, {1: 20}))
 
     def test_sub_affine(self):
         """Test 'sub' function from class Affine"""
-        x = Affine({0: 0, 1: 10})
-        y = Affine({0: 5, 1: 10, 2: 5})
-        self.assertEqual(x - x, Affine({0: 0}))
-        self.assertEqual(x - y, Affine({0: -5, 2: -5}))
+        x = Affine(0, {1: 10})
+        y = Affine(5, {1: 10, 2: 5})
+        self.assertEqual(x - x, Affine(0, {}))
+        self.assertEqual(x - y, Affine(-5, {2: -5}))
         self.assertEqual(x + y - y, x)
 
 
