b495908964140f05b81e0dd8b54f4b5fc5f543e1
zeng <zeng_ruxue@126.com>
2020-04-08 15:16:14 +0200

    test affine avec résultats x0 en chaine de caractère

--- test/test_affine.py
+++ test/test_affine.py
@@ -15,7 +15,7 @@ class TestAffine(unittest.TestCase):
         self.assertEqual(x + y, Affine(5, {1: 20, 2: 5}))
         self.assertEqual(x + x, Affine(0, {1: 20}))
         self.assertEqual(x + 4, Affine(4, {1: 10}))
-        self.assertEqual(x + 7.536 + y, Affine(12.536, {1: 20, 2: 5}))
+        self.assertEqual(x + 7.536 + y, Affine("12.536", {1: 20, 2: 5}))
 
     def test_sub_affine(self):
         """Test 'sub' function from class Affine"""
@@ -35,7 +35,7 @@ class TestAffine(unittest.TestCase):
         self.assertEqual(x * x, Affine(0, {1: 0, 2: 100}))
         # TODO: pb car la conversion en intervalle donne [-100, 100] pour x²
         self.assertEqual(y * 4, Affine(20, {1: 40, 2: 20}))
-        self.assertEqual(y * 7.536, Affine(37.68, {1: 75.36, 2: 37.68}))
+        self.assertEqual(y * 7.536, Affine("37.68", {1: 75.36, 2: 37.68}))
 
     def test_contains_affine(self):
         """Test 'contains' function from class Affine"""
