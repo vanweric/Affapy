a2b9c7df90192c08e06d84f7bab8fa88f4e8b0f3
Quentin18 <quentindeschamps18@orange.fr>
2020-04-30 15:39:37 +0200

    Update docstrings affapyPrecision

--- AffApy/affapyPrecision.py
+++ AffApy/affapyPrecision.py
@@ -10,10 +10,11 @@ class precision(ContextDecorator):
     def __enter__(self, bin_prec=0, dec_prec=0):
         """
         Use to set the mpmath precision for a portion of code
-        :type bin_prec: int
-        :param bin_prec: binary precision of mpmath
-        :type dec_prec: int
-        :param dec_prec: int for decimal precision of mpmath
+
+        Args:
+            bin_prec (int): binary precision of mpmath
+            dec_prec (int): decimal precision of mpmath
+
         """
         precision._precisionStack.append(mp.dps)
         if dec_prec:
@@ -26,9 +27,13 @@ class precision(ContextDecorator):
     def __exit__(self, *exc):
         """
         Reset the mpmath precision to its last value
-        :param exc:
-        :rtype: bool
-        :return: on error
+
+        Args:
+            exc
+
+        Returns:
+            bool: on error
+
         """
         if precision._precisionStack:
             mp.dps = precision._precisionStack.pop()
@@ -41,8 +46,10 @@ class precision(ContextDecorator):
     def precisionDec(self):
         """
         Return decimal precision
-        :rtype: int
-        :return: decimal precision
+
+        Returns:
+            int: decimal precision
+
         """
         return mp.dps
 
@@ -50,8 +57,10 @@ class precision(ContextDecorator):
     def precisionBin(self):
         """
         Return binary precision
-        :rtype: int
-        :return: binary precision
+
+        Returns:
+            int: binary precision
+
         """
         return mp.prec
 
@@ -60,8 +69,10 @@ class precision(ContextDecorator):
     def precisionDec(self, dec_prec):
         """
         Set decimal precision
-        :type dec_prec: int
-        :param dec_prec: decimal precision
+
+        Args:
+            dec_prec (int): decimal precision
+
         """
         mp.dps = dec_prec
 
@@ -69,8 +80,10 @@ class precision(ContextDecorator):
     def precisionBin(self, bin_prec):
         """
         Set binary precision
-        :type  bin_prec: int
-        :param bin_prec: binary precision
+
+        Args:
+            bin_prec (int): binary precision
+
         """
         mp.prec = bin_prec
 
@@ -78,8 +91,10 @@ class precision(ContextDecorator):
     def getOldPrecision():
         """
         Return Older decimal precision
-        :rtype: int
-        :return: last decimal precision
+
+        Returns:
+            int: last decimal precision
+
         """
         if precision._precisionStack:
             return precision._precisionStack[-1]
