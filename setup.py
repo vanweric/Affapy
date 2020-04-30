8df5278dc7377870f4439e6007d6abec9e2023ea
Quentin18 <quentindeschamps18@orange.fr>
2020-04-30 03:19:31 +0200

    Update setup + add __init__

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='AffApy',
-    version='0.1',  # to complete
-    #scripts=['AffApy'],  # to complete
+    version='0.1',
     author=[
         "Ruxue ZENG",
         "Quentin DESCHAMPS",
@@ -16,19 +15,20 @@ setuptools.setup(
     ],
     author_email=[
         "ruxue.zeng@etu.sorbonne-universite.fr",
-        "quentin.deschamps.1@etu.sorbonne-universite.fr",
+        "quentin.deschamps@etu.sorbonne-universite.fr",
         "florian.guily@etu.sorbonne-universite.fr",
         "tristan.michel1@etu.sorbonne-universite.fr"
     ],
     description="A package using affine and interval arithmetic to predict variables content.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/.../AffApy",  # to complete
-    packages=setuptools.find_packages(),
+    url="https://gitlab.lip6.fr/hilaire/affapy",
+    packages=["AffApy"],
     install_requires=['Sphinx', 'mpmath', 'sphinx-rtd-theme'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",  # to complete
         "Operating System :: OS Independent",  # to complete
     ],
+    python_requires='>=3.6',
 )
