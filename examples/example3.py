57d51927fe5dd1641b6a5e5690e5303c9c8061d4
Quentin18 <quentindeschamps18@orange.fr>
2020-05-04 12:47:52 +0200

    Update examples with precision

---
+++ examples/example3.py
@@ -0,0 +1,32 @@
+from AffApy.affapyPrecision import precision
+from AffApy.intervalArithmetic import Interval
+from AffApy.affineArithmetic import Affine
+from mpmath import mp
+
+
+print("== Interval ==")
+with precision(dps=20):
+    x = Interval(-mp.pi, mp.pi)
+    print("x =", x)
+    y = Interval(0, mp.e)
+    print("y =", y)
+    print("x + y =", x + y)
+    print("x - y =", x - y)
+    print("x * y =", x * y)
+    print("exp(x) =", x.exp())
+
+print("== Affine ==")
+with precision(dps=30):
+    x = Affine(interval=[-mp.pi, mp.pi])
+    print("x =", x)
+    y = Affine(interval=[0, mp.e])
+    print("y =", y)
+    print("x + y =", x + y)
+    print("x - y =", x - y)
+    print("x * y =", x * y)
+    print("exp(x) =", x.exp())
+    z = Affine(interval=[-mp.phi, mp.pi])
+    print("z =", z)
+    a = 2*x - y + 4*z
+    print("2x - y + 4z =", a)
+    print("Interval:", a.interval)
