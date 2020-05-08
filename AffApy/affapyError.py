ee28805c9194b4265f6d9e847a02e777198ee701
Quentin18 <quentindeschamps18@orange.fr>
2020-05-08 21:26:11 +0200

    Add warning class

--- AffApy/affapyError.py
+++ AffApy/affapyError.py
@@ -1,6 +1,15 @@
 """Error module"""
+import warnings
 
 
 class AffApyError(Exception):
     """Manage exception errors"""
     pass
+
+
+class AffApyWarning(UserWarning):
+    pass
+
+
+if __name__ == "__main__":
+    warnings.warn('Message warning', AffApyWarning)
